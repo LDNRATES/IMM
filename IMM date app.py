ae0881920806da78d1f1be5ba1b886628f579576
LDNRATES <118908935+LDNRATES@users.noreply.github.com>
2022-11-23 13:59:38 +0000

    Add files via upload

---
+++ IMM date app.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+import streamlit as st
+import datetime as dt
+import calendar as cl
+from ipywidgets import interact
+
+def imm(MONTH,YEAR):
+    if MONTH == 'H':
+        m = 3
+    elif MONTH == 'M':
+        m = 6
+    elif MONTH == 'U':
+        m = 9
+    elif MONTH == 'Z':
+        m = 12
+    else:
+        return 'Not an IMM Month'
+    y = 2000 + int(YEAR)
+    third = dt.date(y,m,15)
+    w = third.weekday()
+    if w != 2:
+        third = third.replace(day = (15 + (2 - w) % 7))
+        
+    return third.strftime('%A, %d %B %Y')
+    
+m = st.selectbox('Month',['H','M','U','Z'])
+y = st.selectbox('Year',[i for i in range(22,100)])    
+st.write(imm(m,y))
+
